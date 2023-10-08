75ad7202fcdafd985c326cb585ffdbec302fa7b4
mekenjitwo <146766240+mekenjitwo@users.noreply.github.com>
2023-10-08 21:53:17 +0300

    create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+if word in meme_dict.keys():
+    print(meme.dict[word])
+else:
+    print('bu kelime sözlükte yok')
+    
